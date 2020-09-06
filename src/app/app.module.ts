e2ed43bac82420c4cbcda4ea025abec0ea2749cb
Erika Hansson <erikahansson_@hotmail.com>
2020-09-06 12:40:51 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeComponent } from './me/me.component';
+import { ReportComponent } from './report/report.component';
+import { WeekComponent } from './week/week.component';
+// import { WeekComponent } from './report/week/week.component';
+// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'me', component: MeComponent },
+  { path: 'reports/week/1', component: ReportComponent },
+  { path: '',
+    redirectTo: '/me',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeComponent,
+    ReportComponent,
+    WeekComponent
+    // WeekComponent,
+    // ReportComponent,
+    // PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
